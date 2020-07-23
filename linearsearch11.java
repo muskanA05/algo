d3df394c8ab31fac5305b1ab88e5ae233c33b3ca
muskanA05 <68376557+muskanA05@users.noreply.github.com>
2020-07-23 10:49:11 +0530

    Update linearsearch11.java

--- linearsearch11.java
+++ linearsearch11.java
@@ -1,5 +1,5 @@
 mport java.util.*;
-class linearsearch
+class linear
 {
  public static void main(String[] args)
  {  Scanner sc =new Scanner (System.in);
@@ -11,15 +11,15 @@ class linearsearch
      }
      int x=sc.nextInt();
      int j;
-     int flag=0;
+     int temp=0;
      for(j = 0; j < n; j++) 
     { 
         if(a[j] == x) 
           { 
-            flag=1;
+            temp=1;
         break;}
     } 
-    if(flag==1)
+    if(temp==1)
     System.out.print("found at"+" "+j);
     else
     System.out.print("not found");
