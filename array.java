4e1262b064d2d8aa92196e255e5e91db30b3dcb2
muskanA05 <68376557+muskanA05@users.noreply.github.com>
2020-07-23 11:07:44 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+import java.util.Scanner;
+ class Arraysort1 {
+    public static void main(String args[])
+    {
+
+        Scanner sc = new Scanner(System.in);
+        int i;
+        int n = sc.nextInt();
+        int a[] = new int[n];
+        for ( i = 0; i < n; i++) {
+            a[i] = sc.nextInt();
+        }
+        Arrays.sort(a);
+        for(int i=0;i<a.length;i++)
+        System.out.println(a[i]);
+
+    }
+}
